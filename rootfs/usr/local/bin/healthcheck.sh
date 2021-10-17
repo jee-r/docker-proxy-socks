c6c8927282febeebb5f5edd593de6a92ad340fba
Jee <jee@jeer.fr>
2021-10-17 15:29:25 +0200

    healtcheck retur ip

--- rootfs/usr/local/bin/healthcheck.sh
+++ rootfs/usr/local/bin/healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --silent --socks5 127.0.0.1:"$LOCALPORT" ifconfig.co
+GETIP=$(curl --silent --socks5 127.0.0.1:"$LOCALPORT" ifconfig.co)
 
 if [ "$?" == 0 ]; then 
   echo "PROXY OK $GETIP" && exit 0
